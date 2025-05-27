64077bb2d63f42c64bfa5abb61ee54e1b6ba7b27
seongminhwan <seongminhwan@example.com>
2025-05-27 18:53:13 +0800

    fix: 在使用处声明err变量

--- main.go
+++ main.go
@@ -336,6 +336,7 @@ func createDialer(cidrList []string, config Config) *net.Dialer {
 
 			// 设置源IP
 			var innerErr error
+			var err error
 			err = c.Control(func(fd uintptr) {
 				// 绑定到指定的源IP
 				if strings.Contains(network, "tcp6") || strings.Contains(network, "udp6") {
