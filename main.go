89fe980397b71025915b162f7ba7844f9ea9c5eb
seongminhwan <seongminhwan@example.com>
2025-05-27 22:59:14 +0800

    fix: 修复不可达的代码

--- main.go
+++ main.go
@@ -389,7 +389,7 @@ func createDialer(cidrList []string, config Config) *net.Dialer {
 
 			// 设置源IP
 			var innerErr error
-			return c.Control(func(fd uintptr) {
+			err := c.Control(func(fd uintptr) {
 				// 绑定到指定的源IP
 				if strings.Contains(network, "tcp6") || strings.Contains(network, "udp6") {
 					innerErr = syscall.SetsockoptIPv6Mreq(int(fd), syscall.IPPROTO_IPV6, syscall.IPV6_MULTICAST_IF, &syscall.IPv6Mreq{})
@@ -402,8 +402,10 @@ func createDialer(cidrList []string, config Config) *net.Dialer {
 					innerErr = syscall.Bind(int(fd), sa)
 				}
 			})
-
-			return innerErr
+			if innerErr != nil && config.Verbose {
+				log.Printf("绑定源IP失败: %v", innerErr)
+			}
+			return err
 		},
 	}
 }
